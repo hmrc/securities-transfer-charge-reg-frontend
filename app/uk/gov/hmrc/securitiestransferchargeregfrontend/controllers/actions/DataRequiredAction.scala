6c67cd1682d02a13eb00fb46e46454b80f78fc2e
jon-humble-hmrc <194776673+jon-humble-hmrc@users.noreply.github.com>
2025-12-31 09:17:41 +0000

    Refactor data required actions

--- app/uk/gov/hmrc/securitiestransferchargeregfrontend/controllers/actions/DataRequiredAction.scala
+++ app/uk/gov/hmrc/securitiestransferchargeregfrontend/controllers/actions/DataRequiredAction.scala
@@ -14,6 +14,22 @@
  * limitations under the License.
  */
 
+/*
+* Copyright 2025 HM Revenue & Customs
+ *
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+ *
+* http://www.apache.org/licenses/LICENSE-2.0
+ *
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
 package uk.gov.hmrc.securitiestransferchargeregfrontend.controllers.actions
 
 import uk.gov.hmrc.securitiestransferchargeregfrontend.models.requests.*
@@ -25,33 +41,27 @@ import uk.gov.hmrc.securitiestransferchargeregfrontend.models.UserAnswers
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-val hasUserAnswers: Option[UserAnswers] => Either[Result, UserAnswers] =
-  case Some(data) => Right(data)
-  case None       => Left(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+def requestOrRedirect[R](maybeUserAnswers: Option[UserAnswers])(onSuccess: UserAnswers => R): Future[Either[Result, R]] =
+  Future.successful(
+    maybeUserAnswers
+      .map(answers => Right(onSuccess(answers)))
+      .getOrElse(Left(Redirect(routes.JourneyRecoveryController.onPageLoad())))
+  )
 
 trait DataRequiredAction extends ActionRefiner[OptionalDataRequest, DataRequest]
 
-class DataRequiredActionImpl @Inject()(implicit val executionContext: ExecutionContext) extends DataRequiredAction {
+class DataRequiredActionImpl @Inject()(implicit val executionContext: ExecutionContext) extends DataRequiredAction:
 
-  override protected def refine[A](request: OptionalDataRequest[A]): Future[Either[Result, DataRequest[A]]] = {
-
-    hasUserAnswers(request.userAnswers) match {
-      case Right(answers) => Future.successful(Right(DataRequest(request.request, request.userId, answers)))
-      case Left(redirect) => Future.successful(Left(redirect))
+  override protected def refine[A](request: OptionalDataRequest[A]): Future[Either[Result, DataRequest[A]]] =
+    requestOrRedirect(request.userAnswers) { answers =>
+      DataRequest(request.request, request.userId, answers)
     }
-  }
-}
 
 trait ValidIndividualDataRequiredAction extends ActionRefiner[ValidIndividualOptionalDataRequest, ValidIndividualDataRequest]
 
-class ValidIndividualDataRequiredActionImpl @Inject()(implicit val executionContext: ExecutionContext) extends ValidIndividualDataRequiredAction {
+class ValidIndividualDataRequiredActionImpl @Inject()(implicit val executionContext: ExecutionContext) extends ValidIndividualDataRequiredAction:
 
-  override protected def refine[A](request: ValidIndividualOptionalDataRequest[A]): Future[Either[Result, ValidIndividualDataRequest[A]]] = {
-
-    hasUserAnswers(request.userAnswers) match {
-      case Right(answers) => Future.successful(Right(ValidIndividualDataRequest(request.request, answers)))
-      case Left(redirect) => Future.successful(Left(redirect))
+  override protected def refine[A](request: ValidIndividualOptionalDataRequest[A]): Future[Either[Result, ValidIndividualDataRequest[A]]] =
+    requestOrRedirect(request.userAnswers) { answers =>
+      ValidIndividualDataRequest(request.request, answers)
     }
-  }
-}
-
