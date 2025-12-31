5c218d53b03f2b86a4cb27a6fd008fa4c4aa499a
zarifskmustafizur-ggs <237820465+zarifskmustafizur-ggs@users.noreply.github.com>
2025-12-31 14:57:00 +0000

    Fixed all the files

--- app/uk/gov/hmrc/securitiestransferchargeregfrontend/controllers/organisations/RegForSecuritiesTransferChargeController.scala
+++ app/uk/gov/hmrc/securitiestransferchargeregfrontend/controllers/organisations/RegForSecuritiesTransferChargeController.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.securitiestransferchargeregfrontend.controllers.organisations
 
 import uk.gov.hmrc.securitiestransferchargeregfrontend.controllers.actions.*
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import uk.gov.hmrc.securitiestransferchargeregfrontend.navigation.Navigator
